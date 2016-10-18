5c58d51a3ee8b7f865347cc4f0b36014c3e1f253
Juan Neri <juanjo.neri@gmail.com>
2016-10-17 17:43:24 -0700

    did not work, check with js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+var video = document.getElementById('video');
+video.addEventListener('click',function(){
+  video.play();
+},false);
